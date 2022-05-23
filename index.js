3f0c3c2066edfcdc27582244d668176ccc5b3040
courtlandcampo <courtland.campo@ufl.edu>
2022-05-23 17:49:31 -0400

    Added questions

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 var questionsArr = [
     {
-        
+        question: 'Anne Hathaway is in "The Devil Wears Prada" (2006)',
+        answer: true;
+        question: 'Anne Hathaway is in "Into The Woods" (2014)',
+        answer: false;
+        question: 'Anne Hathaway is in "Brokeback Mountain" (2005)'
+        answer: true;
+        question: 'Anne Hathaway is in "The Princess Diaries" (2001)'
+        answer: true;
+        question: 'Anne Hathaway is in "La La Land" (2016)',
+        answer: false;
     }
 ]
+
+function runQuiz() {
+    
+}
