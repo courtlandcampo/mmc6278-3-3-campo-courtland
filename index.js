f617247da0cb141ac2371bcf1977f92d8ea9418c
courtlandcampo <courtland.campo@ufl.edu>
2022-05-23 19:17:40 -0400

    Corrected questionsArr placement

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 // Your code here
+function runQuiz() {
+
+}
+
+console.log(questionsArr)
 
 var questionsArr = [
     {
         question: 'Anne Hathaway is in "The Devil Wears Prada" (2006)',
-        answer: true;
-        question: 'Anne Hathaway is in "Into The Woods" (2014)',
-        answer: false;
-        question: 'Anne Hathaway is in "Brokeback Mountain" (2005)'
-        answer: true;
-        question: 'Anne Hathaway is in "The Princess Diaries" (2001)'
-        answer: true;
-        question: 'Anne Hathaway is in "La La Land" (2016)',
-        answer: false;
+        answer: true
+    },
+    {
+        question: 'Anne Hathaway is not in "Into The Woods" (2014)',
+        answer: true
+    },
+    {
+        question: 'Anne Hathaway is in "Brokeback Mountain" (2005)',
+        answer: true
+    },
+    {
+        question: 'Anne Hathaway is in "The Princess Diaries" (2001)',
+        answer: true
+    },
+    {
+        question: 'Anne Hathaway is not in "La La Land" (2016)',
+        answer: true
     }
-]
-
-function runQuiz() {
-    
-}
+];
